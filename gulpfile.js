0cf38e9825a837b39e10154f6098a2cc469ef5da
Rogério da Silva Yokomizo <me@ro.ger.io>
2016-03-11 22:55:25 +0100

    Basic doc layout

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,44 @@
 'use strict';
 
-var gulp = require('gulp');
 var apidoc = require('apidoc');
+var gulp = require('gulp');
+var htmlmin = require('gulp-htmlmin');
+var sass = require('gulp-sass');
+var template = require('gulp-template');
+var through2 = require('through2');
 
-gulp.task('default', [], function () {
+gulp.task('styles', function () {
+  gulp.src('src/style.scss')
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(gulp.dest('dist'));
+});
+
+function loadData() {
   apidoc.createDoc({
     src: '../mosquito-report-api',
-    dest: './.tmp/'
+    dest: '.tmp/'
   });
 
-  var data = require('./.tmp/api_data.json');
+  return require('./.tmp/api_data.json').reduce(function (reduced, x) {
+    reduced[x.group] = reduced[x.group] || {}; 
+    reduced[x.group][x.title] = x;
+
+    return reduced;
+  }, {});
+}
+
+gulp.task('html', function(endCb) {
+
+  console.log(loadData());
+
+  var groupsHtml = '<a href>HTML</a>';
+
+  gulp.src('src/index.html')
+    .pipe(template({ groups: groupsHtml }))
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist'))
+    .on('end', endCb);
+});
+
+gulp.task('default', ['styles', 'html'], function () {
 });
